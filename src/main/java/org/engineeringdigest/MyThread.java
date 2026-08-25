43e03c60efa5798c3f72c0da71c49902980bd401
Karthik VR <karthikvr0312@gmail.com>
2026-08-26 00:40:46 +0530

    Synchronisation

--- src/main/java/org/engineeringdigest/MyThread.java
+++ src/main/java/org/engineeringdigest/MyThread.java
@@ -1,27 +1,15 @@
 package org.engineeringdigest;
 
 public class MyThread extends Thread{
+    private Counter counter;
 
+    MyThread(Counter counter){
+        this.counter=counter;
+    }
     @Override
     public void run() {
-        System.out.println("RUNNING");
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println(e);
+        for (int i=0;i<1000;i++){
+            counter.increment();
         }
-
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        MyThread myThread = new MyThread();
-        System.out.println(myThread.getState());
-        myThread.start();
-        System.out.println(myThread.getState());
-        Thread.sleep(100);
-        System.out.println(myThread.getState());
-        myThread.join();
-        System.out.println(myThread.getState());
     }
 }
--- src/main/java/org/engineeringdigest/Test.java
+++ src/main/java/org/engineeringdigest/Test.java
@@ -1,12 +1,20 @@
 package org.engineeringdigest;
 
-public class Test implements Runnable{
-    @Override
-    public void run() {
-//        System.out.println("Test");
-        for (;;){
-            System.out.println("Test");
-        }
-    }
+public class Test{
 
+    public static void main(String[] args)  {
+        Counter counter = new Counter();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println();
+        System.out.println(counter.getCount());
+    }
 }
